7e4e3216944179eb0685a2531a6b299beec77a92
Lisa <elbondarenko04@gmail.com>
2024-03-10 22:55:07 +0300

    add menue bar

--- src/main/java/com/rgbteam/cmf/view/PrimaryController.java
+++ src/main/java/com/rgbteam/cmf/view/PrimaryController.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
     @FXML
-    private void switchToSecondary() throws IOException {
+    private void switchToSecondView() throws IOException {
         GUIView.setRoot("secondary");
     }
 }
