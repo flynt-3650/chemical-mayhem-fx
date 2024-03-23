6acec04dfb92e45d3f32057ad441d38b5d132d7e
OlejaYasniy <oleg.grishaovich@mail.ru>
2024-03-23 20:40:15 +0300

    added size

--- src/main/java/com/rgbteam/cmf/view/PrimaryViewController.java
+++ src/main/java/com/rgbteam/cmf/view/PrimaryViewController.java
@@ -1,10 +1,13 @@
 package com.rgbteam.cmf.view;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import com.rgbteam.cmf.GeneralFlowController;
 
+import com.rgbteam.cmf.chemistry.Element;
 import javafx.css.CssParser;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -16,6 +19,7 @@ public class PrimaryViewController {
 
     @FXML
     public GridPane myGridPane;
+
     private Button[] buttons;
 
 
@@ -23,12 +27,17 @@ public class PrimaryViewController {
 
     public void initialize() {
 
+        addButton();
+
+    }
+
+    public void addButton(){
         buttons = new Button[119];
         int i = 0;
         for(int row = 1 ; row <= 10; row ++){
             for(int  column = 1 ; column <= 18; column ++){
                 if ((row == 1 && column < 18 && column > 1) || (row == 2 && column < 13 && column > 2) || (row == 3 && column < 13 && column > 2) || (row == 8) || (row == 9 && column < 3) || (row == 10 && column < 3)) {
-                    //myGridPane.add(Box.createRigidArea(new Dimension(10, 10)), column, row);
+
                 } else if ((row == 6 && column == 3)) {
                     i += 15;
                 } else if ((row == 7 && column == 3)) {
@@ -42,14 +51,15 @@ public class PrimaryViewController {
                         break;
                     }
                     buttons[i] = new Button(String.valueOf(i+1));
+                    buttons[i].setPrefSize(70,70);
                     myGridPane.add(buttons[i], column, row);
                     i++;
                 }
                 if (i == buttons.length) break;
             }
         }
-
     }
+
     @FXML
     private void switchToSecondView() throws IOException {
         GUIView.setRoot("secondary");
