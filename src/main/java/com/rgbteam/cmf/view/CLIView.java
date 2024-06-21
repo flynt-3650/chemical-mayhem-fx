7d61cd33b59f1ca9c7a1769059e0bff6a1cbb7ba
fl_3650 <repilovi@gmail.com>
2024-06-21 11:29:56 +0300

    minor update

--- src/main/java/com/rgbteam/cmf/view/CLIView.java
+++ src/main/java/com/rgbteam/cmf/view/CLIView.java
@@ -2,7 +2,6 @@
  * Copyright (c) 2024. RGBTeam
  */
 
-
 package com.rgbteam.cmf.view;
 
 import com.rgbteam.cmf.GeneralFlowController;
@@ -19,18 +18,17 @@ public class CLIView {
 
         System.out.println("Hello User!");
 
-        label:
-        while (true) {
+        label: while (true) {
             System.out.println("""
 
                     Enter:
-                    1 -- get element info by its number (87, 43, etc.)
-                    2 -- get element info by its short name ('H', 'As', etc.)
-                    3 -- get element info by its full name ('Sodium', 'Neon', etc.)
-                    4 -- count mass of a compound (M[Na Cl] = 58.44 g/mol, M[Fe S O 4] = 151.901 g/mol, etc.)
-                    5 -- show oxidation state
-                    6 -- define compound class
-                    0 -- exit the program""");
+                    1 - Get element info by its number (87, 43, etc.)
+                    2 - Get element info by its short name ('H', 'As', etc.)
+                    3 - Get element info by its full name ('Sodium', 'Neon', etc.)
+                    4 - Calculate mass of a compound (M[NaCl] = 58.44 g/mol, M[FeSO4] = 151.901 g/mol, etc.)
+                    5 - Show possible oxidation states
+                    6 - Define compound's class
+                    0 - Exit the program""");
 
             String choice = scanner.nextLine();
 
@@ -107,9 +105,8 @@ public class CLIView {
                             "Enter compound (Separate elements, numbers and parenthesis using ' ' or without spaces): ");
                     String unparsed2 = scanner.nextLine();
                     try {
-                        String pair = controller.retrieveClassOfCompound(unparsed2);
-
-                        System.out.println(pair);
+                        String compoundClass = controller.retrieveClassOfCompound(unparsed2);
+                        System.out.println("Class: " + compoundClass);
                     } catch (Exception e) {
                         System.err.println("Invalid input or element not found: " + e.getCause());
                     }
